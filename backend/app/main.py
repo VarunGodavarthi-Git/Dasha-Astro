b76bf5d59597be1281df164feb0c0a6c748da250
VarunGodavarthi-Git <warungodavarthi1@gmail.com>
2026-05-10 13:55:38 +0530

    updated url in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -58,11 +58,11 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        FRONTEND_ORIGIN,
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "https://dasha-astro-r3z1je8cb-varun-godavarthi-s-projects.vercel.app",
-    ],
+    FRONTEND_ORIGIN,
+    "https://dasha-astro.vercel.app",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
